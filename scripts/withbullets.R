f90075f73c745e95ca941aa2fffe4e7affbc5b20
Hamlin <ptx4@cdc.gov>
2025-06-13 11:57:29 -0400

    dealt with spaces after bullets

---
+++ scripts/withbullets.R
@@ -0,0 +1,45 @@
+output <- tibble::tribble(
+  ~ role, ~ company, ~ when, ~ where, ~ details,
+  "Health Scientist Informatics",
+  "Centers for Disease Control and Prevention",
+  "June 2022 - Current",  "Atlanta, GA",
+   "[] * General informatics for Legionella
+   \\vspace{1mm}",
+  "Bioinformatician II with Division of Bacterial Diseases",
+  "Centers for Disease Control and Prevention,  Contractor with ASRT Inc.",
+  "November 2020 - June 2022",  "Atlanta, GA",  
+  "[] * Interfacing with state public health laboratories (PHL)
+   * Refactored Legionella Species ID tool for broader compatibility and interfacing with Nextflow Tower
+   * Development of method validation plan for Legionella Species ID tool
+   * Phylogenetic and phylodynamic analyses for Legionella isolates of public health concern
+   * Developing or testing scripts and pipelines as necessary
+  * 60-day detail on Data Analytics and Visualization for Vaccine Task Force (April - June 2021)",
+  "Association of Public Health Laboratories (APHL) Bioinformatics Fellow",
+  "Centers for Disease Control and Prevention, Fellow with APHL",
+  "September 2019 - November 2020",  "Atlanta, GA",  
+  "[] * Developed tinselR - an Rshiny application for annotating outbreak trees
+   * Participated in APHL core competency and leadership training
+   * 30-day detail on Routine Reporting for COVID-19 Response (June - July 2020)",
+  "Bensasson Lab Post-Doctoral Researcher",
+  "University of Georgia (UGA)",
+  "October 2017 - August 2019",  "Athens, GA",  
+  "[] * Directed collaborative research with UGA graduate and undergraduate students \\newline
+     \\vspace{1mm} 
+     * Collated and streamlined genome mapping for 1000 Saccharomyces cerevisiae isolates as lab resource \\newline
+     \\vspace{1mm} 
+     * Used iterative approach to assign 400 genomes to well-defined populations using best practices for population genomics and phylogenetics \\newline
+     \\vspace{1mm} 
+     * Resolved haplotypes for Candida albicans genomes with PacBio sequencing",
+  "Moyle Lab Post-Doctoral Researcher",
+  "Indiana University (IU)",
+  "June 2015 - September 2017",  "Bloomington, IN",  
+    "[] * Directed research with faculty, graduate, and undergraduates at IU and with two external universities \\newline
+     \\vspace{1mm} 
+     * Genome-wide study of factors that determine the likelihood of introgression for Solanum species \\newline
+     \\vspace{1mm} 
+     * Streamlined genotyping to generate biological replicates. Validate data accuracy using technical replicates along with statistical analyses \\newline
+     \\vspace{1mm} 
+     * Identified targeting rates and floral trait differences with biological \\& technical replicates in large-scale crossing project via statistical analyses") 
+
+
+output
